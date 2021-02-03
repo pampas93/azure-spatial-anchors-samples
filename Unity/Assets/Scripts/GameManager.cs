1ddc7933af2286ea18d367e4ae16e5bd1165e128
pampas93 <abhijit.93@hotmail.com>
2021-02-02 23:55:39 -0800

    Setting up SpatialNotes with basic state design

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿// Author: Abhijit Srikanth (abhijit.93@hotmail.com)
+
+using UnityEngine;
+
+public enum AppState
+{
+    Start,
+    AnchorScanning,
+    AnchorSelect,
+    Stop,
+    Empty
+}
+
+public enum UserRole
+{
+    Author,
+    User
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { 
+        get {
+            return instance;
+        } 
+    }
+    private static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        SwitchAppMode(AppState.Start);
+    }
+
+    private AppState currState = AppState.Empty;
+    private UserRole currRole;
+
+    public void SwitchAppMode(AppState newState, Object data = null)
+    {
+        Debug.LogFormat("Switch from {0} to {1}", currState.ToString(), newState.ToString());
+        currState = newState;
+
+        switch(currState)
+        {
+            case AppState.Start:
+                {
+                    UIManager.Instance.ShowUserRoleSelection();
+                    break;
+                }
+            case AppState.AnchorScanning:
+                {
+                    Debug.Log("Started scanning");
+                    break;
+                }
+            case AppState.AnchorSelect:
+                {
+                    break;
+                }
+            case AppState.Stop:
+                {
+                    break;
+                }
+            default:
+                {
+                    Debug.LogError("State not implemented");
+                    break;
+                }
+        }
+    }
+
+    public void OnRoleSelected(UserRole role)
+    {
+        currRole = role;
+        SwitchAppMode(AppState.AnchorScanning);
+    }
+}
---
+++ Unity/Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿// Author: Abhijit Srikanth (abhijit.93@hotmail.com)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { 
+        get {
+            return instance;
+        } 
+    }
+
+    private static UIManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ShowUserRoleSelection()
+    {
+
+    }
+
+    public void OnRoleSelected(UserRole role)
+    {
+        GameManager.Instance.OnRoleSelected(role);
+    }
+}
