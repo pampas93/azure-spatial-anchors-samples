0af34d456803be4cf226ee353b897fd107a038e3
pampas93 <abhijit.93@hotmail.com>
2021-02-05 08:25:45 -0700

    Use 3D marker pin instead of demo cube as anchor

---
+++ Unity/Assets/Scripts/MarkerClick.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerClick : MonoBehaviour
+{
+    public Action OnClick;
+
+    private void OnMouseDown()
+    {
+        OnClick?.Invoke();
+    }
+}
